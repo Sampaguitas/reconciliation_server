2472559b792763eac9e6f3065b5b52ab8402d83f
Sampaguitas <timothee.desurmont@sampaguitas.com>
2020-08-27 19:02:42 +0400

    changes

--- routes/user/setAdmin.js
+++ routes/user/setAdmin.js
@@ -5,21 +5,15 @@ const _ = require('lodash');
 
 router.put('/', (req, res) => {
     if (_.isEqual(String(req.user._id), req.body.id)) {
-        return res.status(400).json({ message: 'You are not allowed to change your won role.' });
+        return res.status(400).json({ message: 'You cannot change your own role.' });
+    } else if(!req.user.isAdmin){
+        return res.status(400).json({ message: 'You are not authorised to change this role.' });
     } else {
-        User.findOne({ _id: req.user._id, isAdmin: true }, function(errAuth, resAuth) {
-            if (errAuth) {
-                return res.status(400).json({ message: 'An error has occured.' });
-            } else if (!resAuth) {
-                return res.status(400).json({ message: 'Unauthorized.' });
+        User.findByIdAndUpdate(req.body.id, {isAdmin: req.body.isAdmin}, function(errSet, resSet) {
+            if (errSet || !resSet) {
+                return res.status(400).json({ message: 'Could not update role.' });
             } else {
-                User.findByIdAndUpdate(req.body.id, {isAdmin: req.body.isAdmin}, function(errSet, resSet) {
-                    if (errSet || !resSet) {
-                        return res.status(400).json({ message: 'Could not update role.' });
-                    } else {
-                        return res.status(200).json({ message: 'User role has successfully been updated.' });
-                    }
-                });
+                return res.status(200).json({ message: 'User role has successfully been updated.' });
             }
         });
     }
