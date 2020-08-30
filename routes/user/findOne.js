08bc34d7cd44063104731390272fb53be31e6e0a
Sampaguitas <timothee.desurmont@sampaguitas.com>
2020-08-30 10:31:36 +0400

    changes

--- routes/user/findOne.js
+++ routes/user/findOne.js
@@ -3,15 +3,14 @@ const router = express.Router();
 const User = require('../../models/User');
 
 router.get('/', (req, res) => {
-    const id = req.query.id
-    User.findById(id).populate('opco', 'name')
-    .exec(function (err, user) {
+    const userId = req.user._id;
+    User.findById(userId, function (err, user) {
         if (err) {
-            return res.status(400).json({ message: 'An error has occured.'})
+            return res.status(400).json({ message: 'An error has occured, try refreshing this page.'})
         } if (!user) {
-            return res.status(400).json({ message: 'User does not exist.' });
+            return res.status(400).json({ message: 'Could not retrieve user information, try refreshing this page.' });
         } else {
-            return res.json(user);
+            return res.json({user: user});
         }
     });
 });
