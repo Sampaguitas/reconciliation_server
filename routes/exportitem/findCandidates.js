6b471c26e287ae030a079b0df34ba8c771abb96e
Sampaguitas <timothee.desurmont@sampaguitas.com>
2020-09-23 12:12:47 +0400

    changes

--- routes/exportitem/findCandidates.js
+++ routes/exportitem/findCandidates.js
@@ -5,34 +5,61 @@ const _ = require('lodash');
 
 router.post('/', (req, res) => {
 
-    let { artNr, poNr, exRate, filter, sort } = req.body;
+    let { exRate, filter, sort } = req.body;
 
-    if (!artNr) {
+    if (!filter.artNr) {
         res.status(400).json({message: 'Article number is required'});
-    } else if (!poNr) {
+    } else if (!filter.poNr) {
         res.status(400).json({mesaage: 'PO number is required.'});
     } else {
         ImportItem
         .aggregate([
             {
                 $match: {
-                    artNr: artNr,
-                    poNr: poNr,
+                    artNr: filter.artNr,
+                    poNr: filter.poNr,
+                    country : { $regex: new RegExp(escape(filter.country),'i') },
+                    hsCode : { $regex: new RegExp(escape(filter.hsCode),'i') },
+                }
+            },
+            {
+                $lookup: {
+                    from: "importdocs",
+                    localField: "documentId",
+                    foreignField: "_id",
+                    as: "importdocs"
                 }
             },
             {
                 $addFields: {
-                    fPrice: { $divide: ["$unitPrice", exRate] },
-                    fPriceX: { $toString: "$fPrice" },
+                    decNr: { $ifNull: [ { $arrayElemAt: ["$importdocs.decNr", 0] }, ""] },
+                    boeNr: { $ifNull: [ { $arrayElemAt: ["$importdocs.boeNr", 0] }, ""] },
+                    srNrX: { $toString: "$srNr" },
+                    pcsX: { $toString: "$pcs" },
+                    mtrX: { $toString: "$mtr" },
+                    unitNetWeightX: { $toString: "$unitNetWeight" },
+                    unitGrossWeightX: { $toString: "$unitGrossWeight" },
+                    fPriceX: { $toString: { $divide: ["$unitPrice", exRate] } },
                 }
             },
-            // {
-            //     $match: {
-
-            //     }
-            // },
+            {
+                $match: {
+                    decNr : { $regex: new RegExp(escape(filter.decNr),'i') },
+                    boeNr : { $regex: new RegExp(escape(filter.boeNr),'i') },
+                    srNrX : { $regex: new RegExp(escape(filter.srNr),'i') },
+                    // country : { $regex: new RegExp(escape(filter.country),'i') },
+                    // hsCode : { $regex: new RegExp(escape(filter.hsCode),'i') },
+                    pcsX : { $regex: new RegExp(escape(filter.pcs),'i') },
+                    mtrX : { $regex: new RegExp(escape(filter.mtr),'i') },
+                    unitNetWeightX : { $regex: new RegExp(escape(filter.unitNetWeight),'i') },
+                    unitGrossWeightX : { $regex: new RegExp(escape(filter.unitGrossWeight),'i') },
+                    fPriceX : { $regex: new RegExp(escape(filter.unitPrice),'i') },
+                }
+            },
             {
                 $project: {
+                    decNr: 1,
+                    boeNr: 1,
                     srNr: 1,
                     country: 1,
                     hsCode: 1,
@@ -40,7 +67,7 @@ router.post('/', (req, res) => {
                     mtr: 1,
                     unitNetWeight: 1,
                     unitGrossWeight: 1,
-                    unitPrice: "$fPrice",
+                    unitPrice: { $divide: ["$unitPrice", exRate] },
                 }
             }
             
@@ -59,3 +86,7 @@ router.post('/', (req, res) => {
 });
 
 module.exports = router;
+
+function escape(string) {
+    return !_.isUndefined(string) ? string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') : '';
+}
