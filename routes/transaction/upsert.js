52ac577255140e1dfafe290d044c5a4306528290
Sampaguitas <timothee.desurmont@sampaguitas.com>
2020-10-12 12:58:12 +0400

    changes

--- routes/transaction/upsert.js
+++ routes/transaction/upsert.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Transaction = require('../../models/Transaction');
 const ImportItem = require('../../models/ImportItem');
 const ExportItem = require('../../models/ExportItem');
+const _ = require('lodash');
 
 router.post('/', (req, res) => {
 
@@ -21,41 +22,34 @@ router.post('/', (req, res) => {
                 res.status(400).json({message: 'Could not retreive exportItem information.'});
             } else {
                 ImportItem
-                .findById(importId)
-                .populate('importdoc')
-                .exec(function(errImport, importitem) {
+                .findById(importId, function(errImport, importitem) {
                     if (!!errImport || !importitem) {
                         res.status(400).json({message: 'Could not retreive impotItem information.'});
-                    } else {
-                        let summary = importitem.importdoc.summary.find(element => _.isEqual(element.hsCode, importitem.hsCode) && _.isEqual(element.country, importitem.country));
-                        if (_.isUndefined(summary)) {
-                            res.status(400).json({message: 'Could not retreive import summary information.'});
-                        } else if ( ((summary.totalPrice / summary.pcs) / (exportitem.exportdoc.exRate || 1) ) > exportitem.unitPrice ){
+                    } else if ( (importitem.unitPrice / (exportitem.exportdoc.exRate || 1) ) > exportitem.unitPrice ){
                             res.status(400).json({message: 'Export value should not be less than the import value.'});
+                    } else {
+                        let exportRemPcs = Math.max(exportitem.pcs - (exportitem.assignedPcs || 0), 0);
+                        let exportRemMtr = Math.max(exportitem.mtr - (exportitem.assignedMtr || 0), 0);
+                        let importRemPcs = Math.max(importitem.pcs - (importitem.assignedPcs || 0), 0);
+                        let importRemMtr = Math.max(importitem.mtr - (importitem.assignedMtr || 0), 0);
+                        let pcs = Math.min(exportRemPcs, importRemPcs);
+                        let mtr = Math.min(exportRemMtr, importRemMtr);
+                        if (pcs == 0 && mtr == 0) {
+                            return res.status(400).json({ message: 'It seems that all quantities have already been imported.'});
                         } else {
-                            let exportRemPcs = Math.max(exportitem.pcs - (exportitem.assignedPcs || 0), 0);
-                            let exportRemMtr = Math.max(exportitem.mtr - (exportitem.assignedMtr || 0), 0);
-                            let importRemPcs = Math.max(importitem.pcs - (importitem.assignedPcs || 0), 0);
-                            let importRemMtr = Math.max(importitem.mtr - (importitem.assignedMtr || 0), 0);
-                            let pcs = Math.min(exportRemPcs, importRemPcs);
-                            let mtr = Math.min(exportRemMtr, importRemMtr);
-                            if (pcs == 0 && mtr == 0) {
-                                return res.status(400).json({ message: 'It seems that all quantities have already been imported.'});
-                            } else {
-                                let conditions = { importId, exportId };
-                                let update = { $inc: { pcs, mtr } };
-                                let options = { new: true, upsert: true };
-                                
-                                Transaction.findOneAndUpdate(conditions, update, options, function(err, doc) {
-                                    if (err) {
-                                        return res.status(400).json({ message: 'An error has occured.' });
-                                    } else if (!doc) {
-                                        return res.status(400).json({ message: 'transaction could not be created.' });
-                                    } else {
-                                        return res.status(200).json({ message: 'Transaction successfully created.' });
-                                    }
-                                });
-                            }
+                            let conditions = { importId, exportId };
+                            let update = { $inc: { pcs, mtr } };
+                            let options = { new: true, upsert: true };
+                            
+                            Transaction.findOneAndUpdate(conditions, update, options, function(err, doc) {
+                                if (err) {
+                                    return res.status(400).json({ message: 'An error has occured.' });
+                                } else if (!doc) {
+                                    return res.status(400).json({ message: 'transaction could not be created.' });
+                                } else {
+                                    return res.status(200).json({ message: 'Transaction successfully created.' });
+                                }
+                            });
                         }
                     }
 
